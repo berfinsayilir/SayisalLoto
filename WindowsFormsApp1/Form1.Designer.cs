c26ad2638ede5b3b475a85f09cc6af835f10d24a
Berfin SAYILIR <89409721+berfinsayilir@users.noreply.github.com>
2021-10-21 12:45:02 +0300

    Add files via upload

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.textBox11 = new System.Windows.Forms.TextBox();
+            this.textBox13 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.LinkColor = System.Drawing.Color.Black;
+            this.linkLabel1.Location = new System.Drawing.Point(278, 47);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(128, 20);
+            this.linkLabel1.TabIndex = 0;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "SAYISAL  LOTO";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(61, 159);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(57, 26);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(170, 159);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(57, 26);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(270, 159);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(57, 26);
+            this.textBox3.TabIndex = 3;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(365, 159);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(57, 26);
+            this.textBox4.TabIndex = 4;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(476, 159);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(57, 26);
+            this.textBox5.TabIndex = 5;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(578, 159);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(57, 26);
+            this.textBox6.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(253, 233);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(182, 52);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "SONUÇ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(67, 306);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(133, 20);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "KUPON SONUÇ :";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(61, 401);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(57, 26);
+            this.textBox7.TabIndex = 9;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(170, 400);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(57, 26);
+            this.textBox8.TabIndex = 10;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(270, 400);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(57, 26);
+            this.textBox9.TabIndex = 11;
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(365, 400);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(57, 26);
+            this.textBox10.TabIndex = 12;
+            // 
+            // textBox11
+            // 
+            this.textBox11.Location = new System.Drawing.Point(476, 400);
+            this.textBox11.Name = "textBox11";
+            this.textBox11.Size = new System.Drawing.Size(57, 26);
+            this.textBox11.TabIndex = 13;
+            // 
+            // textBox13
+            // 
+            this.textBox13.Location = new System.Drawing.Point(578, 400);
+            this.textBox13.Name = "textBox13";
+            this.textBox13.Size = new System.Drawing.Size(57, 26);
+            this.textBox13.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(793, 487);
+            this.Controls.Add(this.textBox13);
+            this.Controls.Add(this.textBox11);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.linkLabel1);
+            this.Name = "Form1";
+            this.Text = "SayısalLoto";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.TextBox textBox11;
+        private System.Windows.Forms.TextBox textBox13;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int tutan = 0;
+            int sayi1, sayi2, sayi3, sayi4, sayi5, sayi6 = 0;
+            sayi1 = Convert.ToInt32(textBox1.Text);
+            sayi2 = Convert.ToInt32(textBox2.Text);
+            sayi3 = Convert.ToInt32(textBox3.Text);
+            sayi4 = Convert.ToInt32(textBox4.Text);
+            sayi5 = Convert.ToInt32(textBox5.Text);
+            sayi6 = Convert.ToInt32(textBox6.Text);
+            if (sayi1 < 0 || sayi1 >50 || sayi2 < 0 || sayi2 >50 || sayi3 < 0 || sayi3 >50 || sayi4 < 0 || sayi4 >50 || sayi5 < 0 || sayi5 > 50 || sayi6 < 0 || sayi6 > 50 )
+            {
+                MessageBox.Show("Lütfen 1-49 arası bir değer giriniz.");
+            }
+            else {
+
+                Random r = new Random();
+                int rastgele;
+                int sayac = 0; //sayacı sıfıra eşitliyoruz.
+                int[] sayilar = new int[6];  // 6 elemanlı dizi oluşturdum.
+                while (sayac < 6)
+                {
+                    rastgele = r.Next(1, 50); //1 ile 49 arasında rastgele sayı üretidi.
+
+                    if (Array.IndexOf(sayilar, rastgele) == -1)  //dizinin içinde aynı sayı yoksa
+                    {
+                        sayilar[sayac] = rastgele;
+                        sayac++;
+                    }
+
+                }
+
+                Array.Sort(sayilar);
+                textBox7.Text = sayilar[0].ToString(); //Dizinin 1.elemanını label'a basar.
+                textBox8.Text = sayilar[1].ToString(); 
+                textBox9.Text = sayilar[2].ToString(); 
+                textBox10.Text = sayilar[3].ToString(); 
+                textBox11.Text = sayilar[4].ToString(); 
+                textBox13.Text = sayilar[5].ToString(); 
+
+
+                for (int i = 0; i < 6; i++) //GİRDİĞİMİZ DEĞERLER İLE RANDOM DEĞERLERİ KARŞILAŞTIRIYOR.
+                {
+                    if (textBox1.Text == sayilar[i].ToString())
+                    {
+                        tutan++;
+                    }
+                    else if (textBox2.Text == sayilar[i].ToString())
+                    {
+                        tutan++;
+                    }
+                    else if (textBox3.Text == sayilar[i].ToString())
+                    {
+                        tutan++;
+                    }
+                    else if (textBox4.Text == sayilar[i].ToString())
+                    {
+                        tutan++;
+                    }
+                    else if (textBox5.Text == sayilar[i].ToString())
+                    {
+                        tutan++;
+                    }
+                    else if (textBox6.Text == sayilar[i].ToString())
+                    {
+                        tutan++;
+                    }
+                }
+
+                if (tutan >= 3) 
+                {
+                    MessageBox.Show("Kazandınız " + tutan.ToString() + " Tane Tutturdunuz.");
+                }
+                else
+                {
+                    MessageBox.Show("Kaybettiniz " + tutan + " Tane Tutturdunuz.");
+                }
+
+            }
+
+            
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
